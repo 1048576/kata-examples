3be381383badcbf888754a575ba8800b36af2f35
1048576 <fake1048576@fake.com>
2013-05-31 21:35:55 +0100

    Add a failing test.

---
+++ 1048576-kata-examples/src/test/java/com/x1048576/kata/examples/FibonacciSequenceTest.java
@@ -0,0 +1,11 @@
+package com.x1048576.kata.examples;
+
+public class FibonacciSequenceTest {
+	
+	public void shouldCreateNewFibonacciSequence() {
+		
+		// When
+		new FibonacciSequence();
+	}
+
+}
