2e5366d6e95a14744916e3a3e33d859f57ee4269
1048576 <fake1048576@fake.com>
2013-05-31 21:23:35 +0100

    Initial project

---
+++ 1048576-kata-examples/src/main/java/com/x1048576/kata/examples/App.java
@@ -0,0 +1,13 @@
+package com.x1048576.kata.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ make-project.sh
@@ -0,0 +1,8 @@
+
+mvn --batch-mode archetype:generate \
+  -DarchetypeGroupId=org.apache.maven.archetypes \
+  -DarchetypeArtifactId=maven-archetype-quickstart \
+  -DgroupId=1048576 \
+  -DartifactId=1048576-kata-examples \
+  -Dpackage=com.x1048576.kata.examples \
+  -Dversion=0.0.1-SNAPSHOT
